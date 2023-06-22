a45e3cf7b59e4a82d6d998a7f7c280484c37cf95
Scoolj <scoolj99@gmail.com>
2023-06-22 06:13:07 +0100

    Setup server, and signup and signin client

---
+++ client/src/App.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import {StreamChat} from 'stream-chat'
+import {Chat} from 'stream-chat-react'
+import Cookies from 'universal-cookie'
+import './App.css';
+ 
+import {
+    
+    ChannelContainer, 
+    ChannelListContainer,
+    Auth
+
+}from './components'
+
+
+const cookies = new Cookies()
+
+
+const apiKey = '5cft5bjazvdd';
+
+const authToken = cookies.get("token");
+
+const client = StreamChat.getInstance(apiKey)
+
+
+if(authToken) {
+  client.connectUser({
+    
+    id: cookies.get('userId'),
+    name: cookies.get('username'),
+    fullName: cookies.get('fullName'),
+    image: cookies.get('avatarURL'),
+    hashedPassword: cookies.get('hashedPassword'),
+    phoneNumber: cookies.get('phoneNumber'),
+
+  }, authToken)
+}
+
+
+
+
+const App = () => {
+
+  if(!authToken) return <Auth />
+  return (
+    <div className='app__wrapper'>
+
+        <Chat client={client} theme="team light">
+            <ChannelListContainer 
+            />
+
+            <ChannelContainer 
+            />
+
+        </Chat>
+
+        
+      
+    </div>
+  )
+}
+
+export default App
